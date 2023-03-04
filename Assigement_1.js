0ff6e101d40b79096c2207c446eb84ea8657d9ad
omkesh borse <118654283+omkeshborse@users.noreply.github.com>
2023-03-04 17:54:16 +0530

    Add files via upload

---
+++ Assigement_1.js
@@ -0,0 +1,41 @@
+/* Values and Datatypes */
+
+/* 1 Create a javascript file, using javascript comments, List all the datatypes of Javascript, and specify an example for each of them.
+Ans : 
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Objects
+ */
+
+/* let a = " i am happy to join PW Skills FSWD Course";
+
+let b = 9 ; //interger
+let c = 10.2 //floating value
+let d = Number.POSITIVE_INFINITY
+    console.log(d); //infinity
+
+let e = Number.NEGATIVE_INFINITY;
+    console.log(e);
+let f = NaN;
+    console.log(f);
+
+let g = 1024n;
+let i = true ;
+let h ; //as outut undefined 
+    console.log(h);
+
+let j = null;
+
+let k = Symbol('PW Skills')
+let l = [1 , 2 , " pw"]
+
+let m = {
+    name : "PW skills" , course : "FSWD"
+}
+console.log(m);
+ */
